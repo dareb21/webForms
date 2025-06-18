6348cf5582da89177a83fe5ce26e65a3517a9853
dareb21 <qtin10036@gmail.com>
2025-06-18 16:44:05 -0600

    guardar encuestas

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -24,7 +24,7 @@ class StudentController extends Controller
     public function studentEvaluation(Request $request)
     {
     $noClaseId = $request->query('noClaseId');
-    $claseId = $request->query('claseId');
+    $coursesId = $request->query('courseId');
     $survey = new Survey;
     if($survey->where("status",1)->count() == 1)
     {
@@ -33,17 +33,29 @@ class StudentController extends Controller
         $collectionOptions = QuestionOption::select("id", "option", "question_group_id")
             ->whereIn("question_group_id", $questionGroups->pluck("id"))
             ->get();    
-        }   
+        return view('student.studentEvaluation', compact('noClaseId','coursesId','collectionOptions','questionGroups'));
+        }
+          
     else
      {
             return abort(409,"Lo sentimos, algo no esta funcionando como deberia. Intente mas tarde");
       }
 
-        return view('student.studentEvaluation', compact('noClaseId','coursesId'));
+        
     }
 
     public Function studentSubmit(Request $request)
     {
+         $seleccionados = [];
+
+            foreach ($request->all() as $clave => $valor) {
+             if (str_starts_with($clave, 'option_') && $valor === 'on') {
+             preg_match('/option_(\d+)/', $clave, $match);
+             if (isset($match[1])) {
+            $seleccionados[] = (int) $match[1];
+        }
+    }
+}   
         $survey = Survey::select("id")->where("status", 1)->first();
 
         $surveySubmit=SurveySubmit::create([
@@ -51,12 +63,14 @@ class StudentController extends Controller
         "survey_id"=>$survey->id,
         "user_id"=>1,
       ]);
-            foreach ($request->options as $option) {
-                if ($option->value == True )
-                {
-                    
-                }
+            foreach ($seleccionados as $option) {
+               ResponseSubmit::create([
+                "survey_submit_id"=>$surveySubmit->id,
+                "question_option_id"=>$option,
+               ]);
             }
+    return response()->json(["mensaje" => "guardada"]);
+
     }
 
 

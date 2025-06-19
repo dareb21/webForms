9cfca92e8a498e420b69a8957ecd1fdfae662954
Fer117-usap <fernandossrc@gmail.com>
2025-06-19 11:13:19 -0600

    quitar dd request all de create evaluation

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -95,8 +95,6 @@ public function UnableEvaluation()
     $survey->status = 0;
     $survey->save();
     $i=1;
-    dd($request->all());
-    dd($request->questions);
     foreach($request->questions as $question)
     {
       $group=QuestionGroup::create([

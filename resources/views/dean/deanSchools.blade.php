e69303393c6fc000928048fcc221f5c1bbabdd4d
Fer117-usap <fernandossrc@gmail.com>
2025-07-09 10:24:00 -0600

    nombre escuela segun DB

--- resources/views/dean/deanSchools.blade.php
+++ resources/views/dean/deanSchools.blade.php
@@ -11,8 +11,11 @@
             <div class="w-full h-full mt-6 overflow-x-auto">
                 <div class="w-full flex flex-col items-start pt-3">
                     <div class="flex gap-x-4 flex-wrap py-4">  
-                    <label for="catedraticoBusqueda">Búsqueda por escuela </label>
-                    <input type="text" name="catedraticoBusqueda" id="catedraticoBusqueda" class="shadow-sm ml-2 border-1 border-gray-200">
+                    <label for="schoolSearch">Búsqueda por escuela </label>
+                    <select name="schoolSearch" id="schoolSearch" class="shadow-sm ml-2 border-1 border-gray-200">
+                        <!-- foreach de escuelas -->
+                        <option value="">Escuela de Ciencias Informáticas</option>
+                    </select>
                     <button class="inline-block bg-orange-500 hover:bg-blue-700 hover:cursor-pointer text-white text-center font-bold px-3 rounded">
                         Buscar
                     </button>

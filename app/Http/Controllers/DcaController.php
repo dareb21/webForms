ff6fd10fa21032a5832498f14a4b25c20c2e7e4c
Fer117-usap <fernandossrc@gmail.com>
2025-07-21 08:34:42 -0600

    feat: formatter dcacontroller

--- app/Http/Controllers/DcaController.php
+++ app/Http/Controllers/DcaController.php
@@ -7,37 +7,37 @@ use App\Services\AdminServices;
 
 class DcaController extends Controller
 {
-     private $adminServices;
-  public function __construct(AdminServices $adminServices)
-  {
-      $this->adminServices = $adminServices;
-  }
+    private $adminServices;
+    public function __construct(AdminServices $adminServices)
+    {
+        $this->adminServices = $adminServices;
+    }
     public function dcaDashboard()
-        {
+    {
         $dashboard = $this->adminServices->dashboard();
-     return view("adminDCA.dcaDashboard",compact("dashboard"));
-        }
-        public function dcaResults()
-        {
-$adminResults = $this->adminServices->adminResults();
-$years = Survey::selectRAW("Year(dateStart)")
-    ->distinct()
-    ->get();
- return view("adminDCA.dcaResults",compact("dashboard"));
-    
-        }
-        public function dcaStudenView()
-        {
-$adminStudentView = $this->adminServices->adminStudentView($courseId);
-  $years = Survey::selectRAW("Year(dateStart)")
-      ->distinct()
-      ->get(); 
-  return view('admin.adminStudentView',compact("years","resultados","data"));
-        }
-
-        public function dcaAnswerView()
-        {
-        return $this->adminServices->adminViewAnswer($submitId);
-        }
+        return view("adminDCA.dcaDashboard", compact("dashboard"));
+    }
+    public function dcaResults()
+    {
+        $adminResults = $this->adminServices->adminResults();
+        $years = Survey::selectRAW("Year(dateStart)")
+            ->distinct()
+            ->get();
+        return view("adminDCA.dcaResults", compact("dashboard"));
 
     }
+    public function dcaStudenView()
+    {
+        $adminStudentView = $this->adminServices->adminStudentView($courseId);
+        $years = Survey::selectRAW("Year(dateStart)")
+            ->distinct()
+            ->get();
+        return view('admin.adminStudentView', compact("years", "resultados", "data"));
+    }
+
+    public function dcaAnswerView()
+    {
+        return $this->adminServices->adminViewAnswer($submitId);
+    }
+
+}
